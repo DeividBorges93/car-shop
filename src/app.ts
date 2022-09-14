007be04c28b396e4e627b4a0acca744304cc517f
DeividBorges93 <deividmborges@gmail.com>
2022-09-14 18:34:49 -0300

    feat(build): :building_construction: add express-async-errors to handle async errors, add the /cars route to the app and add errorHandler

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 import express from 'express';
+import 'express-async-errors';
+import errorHandler from './middlewares/error';
+import carRoutes from './routes/cars.routes';
 
 const app = express();
+app.use(express.json());
+app.use(carRoutes);
+
+app.use(errorHandler);
 
 export default app;
