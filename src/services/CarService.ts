9c86dbf999d233a0e1394be7d4d4b6cb0283cf0d
DeividBorges93 <deividmborges@gmail.com>
2022-09-14 18:31:23 -0300

    feat(services): :sparkles: create CarService implements from IService<ICar>

---
+++ src/services/CarService.ts
@@ -0,0 +1,16 @@
+import { IService } from '../interfaces/IService';
+import { ICar } from '../interfaces/ICar';
+import { IModel } from '../interfaces/IModel';
+import carZodSchema from '../Schemas/carZodSchema';
+
+export default class CarService implements IService<ICar> {
+  constructor(private _car:IModel<ICar>) {}
+
+  public async create(obj: ICar): Promise<ICar> {
+    const parsed = carZodSchema.safeParse(obj);
+
+    if (!parsed.success) throw parsed.error;
+
+    return this._car.create(obj);
+  }
+}
