e38da5a958fb5e43fb82c8ecd95c885f3c858cd8
DeividBorges93 <deividmborges@gmail.com>
2022-09-14 19:45:23 -0300

    refactor(routes): :recycle: remove instances and put the creator to use

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import CarController from '../controllers/CarController';
-import CarModel from '../models/CarModel';
-import CarService from '../services/CarService';
+import CreateCarController from '../creaters/createCarController';
 
 const route = Router();
 
-const car = new CarModel();
-const carService = new CarService(car);
-const carController = new CarController(carService);
+const carController = CreateCarController.instantiate();
 
 route.post('/cars', (req, res) => carController.create(req, res));
 
