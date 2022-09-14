45415aedebc11738dbe8f6cb96659ceb9aa065ea
DeividBorges93 <deividmborges@gmail.com>
2022-09-14 19:46:16 -0300

    refactor(tests): :recycle: remove instances and put the creator to use and tests the creation failure case

--- src/tests/unit/models/carModel.test.ts
+++ src/tests/unit/models/carModel.test.ts
@@ -1,17 +1,17 @@
 import * as sinon from 'sinon';
 import { expect } from 'chai';
-import CarModel from '../../../models/CarModel';
+import { ZodError } from 'zod';
+import CreateCarModel from '../../../creaters/createCarModel';
 import { Model } from 'mongoose';
 import {
   carMock,
   carMockWithId
 } from '../mocks/carMocks';
-import { ErrorTypes } from '../../../errors/catalog';
 
 describe('Car Model', () => {
-  const carModel = new CarModel();
+  const carModel = CreateCarModel.instantiate();
 
-  before(async () => {
+  before(() => {
     sinon.stub(Model, 'create').resolves(carMockWithId);
   });
 
@@ -23,6 +23,14 @@ describe('Car Model', () => {
     it('successfully created', async () => {
       const newCar = await carModel.create(carMock);
       expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+
+    it('creation failure', async () => {
+      try {
+        await carModel.create({} as any)
+      } catch (error) {
+        expect(error).to.be.instanceOf(ZodError);
+      }
     })
   });
 
