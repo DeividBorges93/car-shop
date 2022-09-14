48f025b78b5d21ad2b291fc68eb28179c8e5a85c
DeividBorges93 <deividmborges@gmail.com>
2022-09-14 19:47:30 -0300

    feat(tests): :sparkles: creates tests for the cars service layer

---
+++ src/tests/unit/services/carService.test.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+import { ZodError } from 'zod';
+import CreateCarService from '../../../creaters/createCarService';
+import CreateCarModel from '../../../creaters/createCarModel';
+import { Model } from 'mongoose';
+import {
+  carMock,
+  carMockWithId
+} from '../mocks/carMocks';
+
+describe('Car Service', () => {
+  const carModel = CreateCarModel.instantiate();
+  const carService = CreateCarService.instantiate();
+
+  before(() => {
+    sinon.stub(Model, 'create').resolves(carMockWithId);
+  });
+
+  after(() => {
+    sinon.restore();
+  });
+
+  describe('Creating a car', () => {
+    it('successfully created', async () => {
+      const newCar = await carService.create(carMock);
+      expect(newCar).to.be.deep.equal(carMockWithId);
+    });
+
+    it('creation failure', async () => {
+      try {
+        await carService.create({} as any)
+      } catch (error) {
+        expect(error).to.be.instanceOf(ZodError);
+      }
+    })
+  })
+})
