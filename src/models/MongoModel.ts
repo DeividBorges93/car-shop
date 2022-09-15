b6a702d4d0abbf1480e99878b0db614945e90074
DeividBorges93 <deividmborges@gmail.com>
2022-09-15 14:52:16 -0300

    feat(models): :sparkles: add read method to car model

--- src/models/MongoModel.ts
+++ src/models/MongoModel.ts
@@ -11,4 +11,8 @@ export default abstract class MongoModel<T> implements IModel<T> {
   public async create(obj: T): Promise<T> {
     return this._model.create({ ...obj });
   }
+  
+  public async read(): Promise<T[]> {
+    return this._model.find();
+  }
 }
