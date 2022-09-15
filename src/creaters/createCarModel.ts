2f057257c54a423191e6e12f9ba4953e78d7c61c
DeividBorges93 <deividmborges@gmail.com>
2022-09-15 18:32:17 -0300

    refactor(creators): :recycle: change the name from class

--- src/creaters/createCarModel.ts
+++ src/creaters/createCarModel.ts
@@ -1,6 +1,6 @@
 import CarModel from '../models/CarModel';
 
-export default class CreateCarController {
+export default class CreateCarModel {
   static instantiate() {
     return new CarModel();
   }
