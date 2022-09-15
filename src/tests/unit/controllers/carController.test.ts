fe1cf8f54fef4608924caa47337b323360604bb8
DeividBorges93 <deividmborges@gmail.com>
2022-09-15 20:20:18 -0300

    refactor(tests): :art: instantiates the controller using the test's service
    puts the stub inside the respective "it"
    
    and removes test from non-existent failure case

--- src/tests/unit/controllers/carController.test.ts
+++ src/tests/unit/controllers/carController.test.ts
@@ -3,8 +3,7 @@ import * as sinon from 'sinon';
 import { ZodError } from 'zod';
 import { Request, Response } from 'express';
 import CreateCarService from '../../../creaters/createCarService';
-import CreateCarController from '../../../creaters/createCarController';
-import { Model } from 'mongoose';
+import CarController from '../../../controllers/CarController';
 import {
   carMock,
   carMockFailure,
@@ -13,23 +12,23 @@ import {
 
 describe('Car Controller', () => {
   const carService = CreateCarService.instantiate();
-  const carController = CreateCarController.instantiate();
+  const carController = new CarController(carService);
   const req = {} as Request;
   const res = {} as Response;
 
   before(() => {
-    sinon.stub(Model, 'create').resolves(carMockWithId);
-
     res.status = sinon.stub().returns(res);
     res.json = sinon.stub().returns(res);
   });
 
-  after(()=>{
+  afterEach(()=>{
     sinon.restore();
   })
 
   describe('Creating a car', () => {
+    
     it('successfully created', async () => {
+      sinon.stub(carService, 'create').resolves(carMockWithId);
       req.body = carMock;
       await carController.create(req, res);
       
@@ -39,15 +38,5 @@ describe('Car Controller', () => {
       expect(status.calledWith(201)).to.be.true;
       expect(json.calledWith(carMockWithId)).to.be.true;
     });
-
-    it('creation failure', async () => {
-      try {
-        req.body = carMockFailure;
-        await carController.create(req, res);
-      } catch (error) {
-        expect(error).to.be.instanceOf(ZodError);
-      }
-    })
   });
-
 });
