425dee5c2c3b6b696429bc304874875ce792fa03
DeividBorges93 <deividmborges@gmail.com>
2022-09-15 20:14:22 -0300

    feat(tests): :sparkles: create const by mocking an array of cars

--- src/tests/unit/mocks/carMocks.ts
+++ src/tests/unit/mocks/carMocks.ts
@@ -28,4 +28,25 @@ const carMockFailure: ICar = {
   doorsQty: 2
 }
 
-export { carMock, carMockWithId, carMockFailure };
+const carArrayMock: ICar[] & { _id: string }[] = [
+  {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  },
+  {
+    _id: "4edd40c94762e0fb12000003",
+    model: "Porsche Cayman",
+    year: 2015,
+    color: "yellow",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+  }
+]
+
+export { carMock, carMockWithId, carMockFailure, carArrayMock };
