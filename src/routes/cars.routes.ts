fa9fdb8b4dd318ccc87af8a362a58c0b97a65f38
DeividBorges93 <deividmborges@gmail.com>
2022-09-15 14:53:46 -0300

    feat(routes): :sparkles: add get route to /cars address

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -6,5 +6,6 @@ const route = Router();
 const carController = CreateCarController.instantiate();
 
 route.post('/cars', (req, res) => carController.create(req, res));
+route.get('/cars', (req, res) => carController.read(req, res));
 
 export default route;
